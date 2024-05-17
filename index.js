81c8840aca2debfb2f4b52d26f1c81ea237a793c
LE1NART <Linus@von-Au.de>
2024-05-17 11:30:17 +0200

    feddich

--- index.js
+++ index.js
@@ -1,13 +1,190 @@
 const fragen = [{
     "fragenId":1,
-    "fragenText":"Dies das",
-    "fragenAntwort":"Haha jaja"
+    "fragenText":"Was ist Jans vollständiger Bürgerlicher Name? Und wie wird der Nachname Buchstabiert?",
+    "fragenAntwort":"Jan-Philipp Waellnitz"
 },
 {
     "fragenId":2,
-    "fragenText":"Dies und jenes",
-    "fragenAntwort":"Haha neenee"
-}];
+    "fragenText":"Wie hieß der Studiengang den Jan erfolgreich abgeschlossen hat?",
+    "fragenAntwort":"Elektrotechnik"
+},
+{
+    "fragenId":3,
+    "fragenText":"Was war das erste Haustier von Jan?",
+    "fragenAntwort":"Ein Gekko names George"
+},
+{
+    "fragenId":4,
+    "fragenText":"Schätzfrage: Wann haben sich Jan und Sophie verlobt?",
+    "fragenAntwort":"12.06.2023"
+},
+{
+    "fragenId":5,
+    "fragenText":"Schätzfrage: Wann ist Jan geboren? Mit Uhrzeit.",
+    "fragenAntwort":"11.03.97 21:01 Uhr"
+},
+{
+    "fragenId":6,
+    "fragenText":"In welchen Jahren war Jan in den USA?",
+    "fragenAntwort":"2013-2014"
+},
+{
+    "fragenId":7,
+    "fragenText":"Was ist Jans Gaming Tag?",
+    "fragenAntwort":"NerVix"
+},
+{
+    "fragenId":8,
+    "fragenText":"Was ist der Jahrestag von Jan und Sophie?",
+    "fragenAntwort":"17.07.22"
+},
+{
+    "fragenId":9,
+    "fragenText":"Was ist der höchste Berg Asiens?",
+    "fragenAntwort":"Der Mount Everest mit 8848m"
+},
+{
+    "fragenId":10,
+    "fragenText":"Welches dieser Länder gehört nicht zu Afrika? <br> a) Algerien<br>b)Paraguay<br>c)Niger<br>d)Kamerun",
+    "fragenAntwort":"b) Paraguay"
+},
+{
+    "fragenId":11,
+    "fragenText":"SCHÄTZFRAGE:<br>Wie lange dauert es, bis der Jupiter die Sonne umrundet hat? (In Jahre,Tage und Stunden)",
+    "fragenAntwort":"11 Jahre 315 Tage und 3 Stunden"
+},
+{
+    "fragenId":12,
+    "fragenText":"Was ist ein Oxymoron?<br>a) Ein Versmaß<br>b) Eine geometrische Form<br>c)Wörter welche aus komplett gegensätzlichen Wörtern zusammmen gesetzt werden<br>d)Wiederholung desselben Wortes oder derselben Wortgruppe am Anfang aufeinanderfolgender Sätze oder Satzteile. ",
+    "fragenAntwort":"Wörter welche aus komplett gegensätzlichen Wörtern zusammmen gesetzt werden. z.B. Hassliebe"
+},
+{
+    "fragenId":13,
+    "fragenText":"Von welchem Land ist Quito die Hauptstadt?",
+    "fragenAntwort":"Ecuador"
+},
+{
+    "fragenId":14,
+    "fragenText":"Er soll Frauen zur besonderen Lust verhelfen: Der G-Punkt. Wofür steht das G beim G-Punkt?",
+    "fragenAntwort":"Für den 'Entdecker' des G-Punktes namens Gräfenberg"
+},
+{
+    "fragenId":15,
+    "fragenText":"Wie viel ml Flüssigkeit werden im Durchschnitt beim männlichen Samenerguss ausgeschieden?<br>a)2-6ml<br>b)20-40ml<br>c)50-70ml<br>d100-150ml",
+    "fragenAntwort":"Etwa 2 bis 6 ml"
+},
+{
+    "fragenId":16,
+    "fragenText":"Sex in der Schwangerschaft- bis zu welchem Monat ist das okay?",
+    "fragenAntwort":"Solange es keine Risikoschwangerschaft ist, ist Sex erlaubt."
+},
+{
+    "fragenId":17,
+    "fragenText":"Was ist ein Kontakthof?",
+    "fragenAntwort":"Vorraum bei einem Bordell in dem Prosteturierte mit Kunden in Kontakt kommen."
+},
+{
+    "fragenId":18,
+    "fragenText":"Was ist Frotteurismus?<br>a)Erregung durch Kälte<br>b) Ein Fetisch bezogen auf das Tragen von Frotee-Kleidung<br>c) Der Fachausdruck für ein indisches Hochzeitsritual vor der Liebesnacht<br>d) Das spüren sexueller Lust beim unbemerkten Reiben am Körper eines anderen Menschens",
+    "fragenAntwort":"d) Das unbemerkte Reiben an anderen Menschen"
+},
+{
+    "fragenId":19,
+    "fragenText":"Nennen einen der drei Filme, welcher elf Oscars gewann.",
+    "fragenAntwort":"Ben Hur 1960<br>Titanic 1998<br>Der Herr der Ringe: Die Rückkehr des Königs 2004"
+},
+{
+    "fragenId":20,
+    "fragenText":"Wer spielte den ersten James Bond?",
+    "fragenAntwort":"Sean Connery"
+},
+{
+    "fragenId":21,
+    "fragenText":"Welche ist die längste Serie der Welt, nach Folgen?",
+    "fragenAntwort":"Das Sandmänchen"
+},
+{
+    "fragenId":22,
+    "fragenText":"Wie werden Dinge in Filmen genannt, die die Handlung auslösen oder vorantreiben, aber für das Publikum keine bedeutung haben.",
+    "fragenAntwort":"McGuffin z.B. Der Koffer in Pulp Fiction oder der heilige Gral in Die Ritter der Kokusnuß"
+},
+{
+    "fragenId":23,
+    "fragenText":"In der Serie Prinz von Bel Air spielt Wil Smith die Rolle Wil Smith, aber wofür steht 'Wil' beim Schauspieler und für welchen Namen bei der Rolle?",
+    "fragenAntwort":"Der Schauspieler heißt Willard Smith und die Rolle heißt William Smith"
+},
+{
+    "fragenId":24,
+    "fragenText":"Welche/r Künstler*In/Band hat die meisten verkauften Tonträger? Streaming ausgeschlossen!<br>a)Elvis Presley<br>b)Taylor Swift<br>c)Madonna<br>d)The Beatles",
+    "fragenAntwort":"Die Beatles"
+},
+{
+    "fragenId":25,
+    "fragenText":"Welche Rockband hat bisher als einzige Band Weltweit auf allen sieben Kontinenten ein Konzert gegeben?",
+    "fragenAntwort":"Metallica"
+},
+{
+    "fragenId":26,
+    "fragenText":"Welches Lied wurde mit acht Milliarden am häufigsten auf YouTube geschaut?",
+    "fragenAntwort":"Despacito"
+},
+{
+    "fragenId":27,
+    "fragenText":"Ordne die folgenden Komponisten den Werken zu.a)Vivaldi<br>b)Beethoven<br>c)Mozart<br>d)Bach<br>zuordnen zu<br>1)Die Königin der Nacht<br>2)Ode an die Freude<br>3)Die Johannispassion<br>4)Sommer<br> ",
+    "fragenAntwort":"Vivaldi- Sommer<br>Beethoven - Die Ode an die Freude<br>Mozart- Die Königin der Nacht<br>Bach - Die Johannispassion"
+},
+{
+    "fragenId":28,
+    "fragenText":"Das älteste Instrument der Welt wurde 2009 auf der schwäbischen Alb entdeckt. Worum handelt es sich dabei und woraus besteht es?",
+    "fragenAntwort":"Flöte aus (Vogel-)Knochen"
+},
+{
+    "fragenId":29,
+    "fragenText":"In welchem Jahr war die Wiedervereinigung Deutschlands?",
+    "fragenAntwort":"1990"
+},
+{
+    "fragenId":30,
+    "fragenText":"Schätzfrage: Wie viele Eingänge hat das Kolosseum in Rom?",
+    "fragenAntwort":"80"
+},
+{
+    "fragenId":31,
+    "fragenText":"Wie alt wurde Adolf Hitler?",
+    "fragenAntwort":"56 Jahre, 1889-1945"
+},
+{
+    "fragenId":32,
+    "fragenText":"Welche Farbe hatte die Freiheitsstatue ursprünglich?",
+    "fragenAntwort":"Bronze"
+},
+{
+    "fragenId":33,
+    "fragenText":"Nenne 5 der sieben Weltwunder der Antike.",
+    "fragenAntwort":"1. Die hängenden Gärten von Babylon<br>2. Der Koloss von Rhodos<br>3. Das Mausoleum von Halikarnos<br>4. Der Leuchtturm von Alexandria<br>5. Die Pyramiden von Gizeh<br>6. Der Tempel von Artemis<br>7. Die Zeus-Statue von Phidias"
+},
+{
+    "fragenId":34,
+    "fragenText":"Eine Zündene Idee: Zünde das Streichholz an, das erste Team gewinnt.  Feuerzeuge sind nicht erlaubt.",
+    "fragenAntwort":""
+},
+{
+    "fragenId":35,
+    "fragenText":"Findet Waldo: BO3 ",
+    "fragenAntwort":""
+},
+{
+    "fragenId":36,
+    "fragenText":"Nicht lachen: Das Team was zu erst lacht verliert. Von jetzt an.",
+    "fragenAntwort":""
+},
+{
+    "fragenId":37,
+    "fragenText":"Zwei Wahrheiten eine Lüge(3Pkt): Beide Teams erzählen zwei wahre Geschichten die ihnen passiert sind und eine Lüge, das andere Team rät was die Lüge ist. ",
+    "fragenAntwort":""
+},
+
+];
 
 
 function button(id){
